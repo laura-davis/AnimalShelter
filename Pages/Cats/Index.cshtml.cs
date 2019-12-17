5f90ab9c87eaba15625ca73d83761a352f5e5492
Lakitu Team <Lakitu@sky.uk>
2019-12-17 22:03:25 +0000

    make cats/index use paginatedList

--- Pages/Cats/Index.cshtml.cs
+++ Pages/Cats/Index.cshtml.cs
@@ -1,4 +1,5 @@
-using System.Collections.Generic;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using DogShelter.Data;
 using DogShelter.Models;
@@ -16,11 +17,56 @@ namespace DogShelter.Pages.Cats
             _context = context;
         }
 
-        public IList<Cat> Cat { get; set; }
+        public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
 
-        public async Task OnGetAsync()
+        public PaginatedList<Cat> Cats { get; set; }
+
+        public async Task OnGetAsync(string sortOrder,
+            string currentFilter, string searchString, int? pageIndex)
         {
-            Cat = await _context.Cats.ToListAsync();
+            CurrentSort = sortOrder;
+            NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                pageIndex = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            CurrentFilter = searchString;
+
+            IQueryable<Cat> catsIq = from s in _context.Cats
+                select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                catsIq = catsIq.Where(d => d.Name.Contains(searchString));
+            }
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    catsIq = catsIq.OrderByDescending(d => d.Name);
+                    break;
+                case "Date":
+                    catsIq = catsIq.OrderBy(d => d.Dob);
+                    break;
+                case "date_desc":
+                    catsIq = catsIq.OrderByDescending(d => d.Dob);
+                    break;
+                default:
+                    catsIq = catsIq.OrderBy(d => d.Name);
+                    break;
+            }
+
+            int pageSize = 3;
+            Cats = await PaginatedList<Cat>.CreateAsync(
+                catsIq.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
