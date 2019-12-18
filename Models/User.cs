f6f47378334fcbad570fb317ddf7bd9bcd04a497
Lakitu Team <Lakitu@sky.uk>
2019-12-18 18:01:06 +0000

    add annotations to models

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,37 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogShelter.Models
 {
     public class User
     {
         public int ID { get; set; }
+        
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        
+        [Required]
+        [Display(Name = "Address")]
         public string AddressLine1 { get; set; }
+        
+        [Required]
+        [Display(Name = "Postcode")]
         public string Postcode { get; set; }
+        
+        [Required]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+        
+        [Required]
+        [Display(Name = "Phone number")]
         public string Telephone { get; set; }
+        
+        [Required]
         public bool IsAdmin { get; set; }
 
         public ICollection<Adoption> Adoptions { get; set; }
